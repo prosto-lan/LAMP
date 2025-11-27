358769a37ec761ad84f4161fb6852d3802222a7b
prosto_lan <blacksomebarte@gmail.com>
2025-11-27 11:16:21 +0500

    first_commit

---
+++ src/APITester.tsx
@@ -0,0 +1,10 @@
+import { useRef, type FormEvent } from "react";
+
+export function APITester() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { APITester } from "./APITester";
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="app">
+      <APITester />
+    </div>
+  );
+}
+
+export default App;
